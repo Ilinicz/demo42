aa80ffd3d987878b71bc48bdcd56e721efe8e4a5
vilinicz <vilinicz@mail.ru>
2016-01-19 00:13:19 +0300

    Automated commit at 2016-01-18 21:13:19 UTC by middleman-deploy 2.0.0-alpha

--- config.rb
+++ config.rb
@@ -2,44 +2,20 @@
 # Page options, layouts, aliases and proxies
 ###
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+activate :directory_indexes
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
 
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+ activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  # Relative assets needed to deploy to Github Pages
   activate :relative_assets
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
 
 activate :deploy do |deploy|
