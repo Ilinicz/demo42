268fe7128f9b866e3a10e83eeeef09e63cd2aaa0
vilinicz <vilinicz@mail.ru>
2016-02-24 03:34:43 +0300

    dark-version

--- config.rb
+++ config.rb
@@ -3,11 +3,13 @@
 ###
 
 activate :directory_indexes
+#activate :autoprefixer
 
 set :relative_links, true
 #set :css_dir, "assets/stylesheets"
 #set :js_dir, "assets/javascripts"
 #set :images_dir, "assets/images"
+#set :fonts_dir, 'assets/fonts'
 
 configure :development do
  activate :livereload
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-//= require_tree .
+$( document ).ready(function() {
+var header = $("#sticky");
+var headerY = header.offset().top;
+$(document).scroll(function () {
+    var y = $(document).scrollTop();
+
+    if (y >= headerY) {
+        header.addClass('fixed');
+    } else {
+        header.removeClass('fixed');
+    }
+});
+});
